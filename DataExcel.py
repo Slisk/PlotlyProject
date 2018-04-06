5c2d05d891901649e4fdefa2517f747744e4e4bf
Johan Moback <Johan.Moback@datscha.com>
2018-04-06 13:29:01 +0200

    first commit

---
+++ DataExcel.py
@@ -0,0 +1,26 @@
+import sys
+import os 
+import pandas as pd
+import numpy as np
+import plotly.plotly as py 
+
+
+df = pd.read_excel('C:\Users\johmob01\Documents\VTPdiff\DiffRapport_Newsec.xls', sheet_name='Samtliga kommuner och lokalslag')  
+
+
+
+
+
+
+
+
+
+
+#location = r'C:\Users\johmob01\Documents\VTPdiff\DiffRapport_Newsec.xls'
+#df = pd.read_excel(location, 0, names='Kommun') 
+#df.dtypes 
+#df.head() 
+
+
+
+
